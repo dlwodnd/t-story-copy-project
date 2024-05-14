dafa0a4e6d24d73b05095faa8abc5588a69e1927
leejaewoong <leejaewoong300@gmail.com>
2024-05-14 16:44:49 +0900

    개인 프로젝트 전에 작성한 코드(수정 필요) 옮기기 완료 2024-05-14

---
+++ src/main/java/com/projcet/tstorycopyproject/domain/catagory/controller/CategoryController.java
@@ -0,0 +1,14 @@
+package com.projcet.tstorycopyproject.domain.catagory.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/category")
+public class CategoryController {
+
+}
---
+++ src/main/java/com/projcet/tstorycopyproject/domain/catagory/service/CategoryService.java
@@ -0,0 +1,12 @@
+package com.projcet.tstorycopyproject.domain.catagory.service;
+
+import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Server
+@Slf4j
+@RequiredArgsConstructor
+public class CategoryService {
+
+}
